62280b56c729b8b82a1b6f51ad63c8278eb37be6
Rafael Sá <rafael.sa.menezes40@gmail.com>
2020-02-03 11:15:18 -0400

    Update merge-results.sh

--- merge-results.sh
+++ merge-results.sh
@@ -2,7 +2,7 @@ mkdir results
 cp merge-results.xml results
 cd results
 
-_CATEGORIES=("ReachSafety-Arrays" "ReachSafety-BitVectors" "ReachSafety-ControlFlow" "ReachSafety-Floats" "ReachSafety-Heap" "ReachSafety-Loops" "ReachSafety-ProductLines" "ReachSafety-Recursive" "ConcurrencySafety-Main" "MemSafety-Arrays" "MemSafety-Heap" "MemSafety-LinkedLists" "MemSafety-Other" "MemSafety-MemCleanup" "SoftwareSystems-BusyBox-MemSafety" "SoftwareSystems-OpenBSD-MemSafety" "NoOverflows-BitVectors" "Termination-MainHeap" "Termination-Other" "ReachSafety-ECA" "ReachSafety-Sequentialized" "SoftwareSystems-AWS-C-Common-ReachSafety" "SoftwareSystems-DeviceDriversLinux64-ReachSafety" "MemSafety-TerminCrafted")
+_CATEGORIES=("ReachSafety-Arrays" "ReachSafety-BitVectors" "ReachSafety-ControlFlow" "ReachSafety-Floats" "ReachSafety-Heap" "ReachSafety-Loops" "ReachSafety-ProductLines" "ReachSafety-Recursive" "ConcurrencySafety-Main" "MemSafety-Arrays" "MemSafety-Heap" "MemSafety-LinkedLists" "MemSafety-Other" "MemSafety-MemCleanup" "SoftwareSystems-BusyBox-MemSafety" "SoftwareSystems-OpenBSD-MemSafety" "NoOverflows-BitVectors" "NoOverflows-Other" "Termination-MainHeap" "Termination-MainControlFlow" "Termination-Other" "ReachSafety-ECA" "ReachSafety-Sequentialized" "SoftwareSystems-AWS-C-Common-ReachSafety" "SoftwareSystems-DeviceDriversLinux64-ReachSafety" "SoftwareSystems-BusyBox-NoOverflows" "MemSafety-TerminCrafted")
 
 function prepare_files {
     CATEGORY="$1"
